7430932c8874cccb6e17b69cd8bc7146cf7d11a5
Elias Dobrin <elias@dobrin.net>
2024-08-14 14:28:28 -0700

    reformatted code.

--- src/brain.c
+++ src/brain.c
@@ -2,72 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv)
-{
-	if(argc != 2)
-	{
+int main(int argc, char **argv) {
+	if (argc != 2) {
 		fprintf(stderr, "(@): call error (%s)\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	FILE *file = fopen(argv[1], "r");
-	if(file == NULL)
-	{
+	if (file == NULL) {
 		fprintf(stderr, "(@): open error (%s)\n", argv[1]);
 		return EXIT_FAILURE;
 	}
-	if(fseek(file, 0, SEEK_END) != 0)
-	{
+	if (fseek(file, 0, SEEK_END) != 0) {
 		fprintf(stderr, "(@): seek error (%s)\n", argv[1]);
 		return EXIT_FAILURE;
 	}
 	int size = ftell(file);
-	if(size == -1)
-	{
+	if (size == -1) {
 		fprintf(stderr, "(@): size error (%s)\n", argv[1]);
 		return EXIT_FAILURE;
 	}
-	if(fseek(file, 0, SEEK_SET) != 0)
-	{
+	if (fseek(file, 0, SEEK_SET) != 0) {
 		fprintf(stderr, "(@): seek error (%s)\n", argv[1]);
 		return EXIT_FAILURE;
 	}
 	char *prog = malloc(size);
-	if(prog == NULL)
-	{
+	if (prog == NULL) {
 		fprintf(stderr, "(@): alloc error (%dB)\n", size);
 		return EXIT_FAILURE;
 	}
-	if(fread(prog, 1, size, file) < size)
-	{
+	if (fread(prog, 1, size, file) < size) {
 		fprintf(stderr, "(@): read error (%s)\n", argv[1]);
 		return EXIT_FAILURE;
 	}
-	if(fclose(file) != 0)
-	{
+	if (fclose(file) != 0) {
 		fprintf(stderr, "(@): close error (%s)\n", argv[1]);
 		return EXIT_FAILURE;
 	}
 	int bulk = 1;
 	char *tape = malloc(bulk);
-	if(tape == NULL)
-	{
+	if (tape == NULL) {
 		fprintf(stderr, "(@): alloc error (%dB)\n", bulk);
 		return EXIT_FAILURE;
 	}
 	memset(tape, 0, bulk);
 	char *cell = tape;
 	int i = 0;
-	for(char *inst = prog; inst < prog + size; inst++)
-	{
-		switch(*inst)
-		{
+	for (char *inst = prog; inst < prog + size; inst++) {
+		i++;
+		switch (*inst) {
 			case '>':
 				cell++;
-				if(cell >= tape + bulk)
-				{
+				if (cell >= tape + bulk) {
 					char *swap = realloc(tape, bulk * 2);
-					if(swap == NULL)
-					{
+					if (swap == NULL) {
 						fprintf(stderr, "(@): realloc error (%dB)\n", bulk * 2);
 						return EXIT_FAILURE;
 					}
@@ -78,11 +65,9 @@ int main(int argc, char** argv)
 				break;
 			case '<':
 				cell--;
-				if(cell < tape)
-				{
+				if (cell < tape) {
 					char *swap = realloc(tape, bulk * 2);
-					if(swap == NULL)
-					{
+					if (swap == NULL) {
 						fprintf(stderr, "(@): realloc error (%dB)\n", bulk * 2);
 						return EXIT_FAILURE;
 					}
@@ -95,32 +80,24 @@ int main(int argc, char** argv)
 				break;
 			case '+':
 				(*cell)++;
-				if(*cell >= 256)
-				{
-					*cell %= 256;
+				if (*cell >= 0x100) {
+					*cell %= 0x100;
 				}
 				break;
 			case '-':
 				(*cell)--;
-				if(*cell < 0)
-				{
-					*cell %= 256;
+				if (*cell < 0) {
+					*cell %= 0x100;
 				}
 				break;
 			case '[':
-				if(*cell == 0)
-				{
-					for(int loop = 0; inst < prog + size; inst++)
-					{
-						if(*inst == '[')
-						{
+				if (*cell == 0) {
+					for (int loop = 0; inst < prog + size; inst++) {
+						if (*inst == '[') {
 							loop++;
-						}
-						else if(*inst == ']')
-						{
+						} else if (*inst == ']') {
 							loop--;
-							if(loop == 0)
-							{
+							if (loop == 0) {
 								break;
 							}
 						}
@@ -128,19 +105,13 @@ int main(int argc, char** argv)
 				}
 				break;
 			case ']':
-				if(*cell != 0)
-				{
-					for(int loop = 0; inst >= prog; inst--)
-					{
-						if(*inst == ']')
-						{
+				if (*cell != 0) {
+					for (int loop = 0; inst >= prog; inst--) {
+						if (*inst == ']') {
 							loop++;
-						}
-						else if(*inst == '[')
-						{
+						} else if (*inst == '[') {
 							loop--;
-							if(loop == 0)
-							{
+							if (loop == 0) {
 								break;
 							}
 						}
@@ -153,8 +124,17 @@ int main(int argc, char** argv)
 			case ',':
 				*cell = fgetc(stdin);
 				break;
+			case '#':
+				printf("position within code: %d\n", (int) (inst - prog));
+				printf("pointer: %d\n", (int) (cell - tape));
+				printf("value at pointer: %d\n", (int) *cell);
+				break;
+			default:
+				i--;
+				break;
 		}
 	}
+	printf("finished instructions: %d\n", i);
 	free(prog);
 	free(tape);
 	return EXIT_SUCCESS;
