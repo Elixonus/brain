94120942db353ca3a5ae4019ff3fdbf3c4ca70b5
Elias Dobrin <elias@dobrin.net>
2024-08-15 14:09:15 -0700

    fixed memory bug causing new memory to not be zeroed.

--- src/brain.c
+++ src/brain.c
@@ -5,50 +5,48 @@
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "(@): call error (%s)\n", argv[0]);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	FILE *file = fopen(argv[1], "r");
 	if (file == NULL) {
 		fprintf(stderr, "(@): open error (%s)\n", argv[1]);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	if (fseek(file, 0, SEEK_END) != 0) {
 		fprintf(stderr, "(@): seek error (%s)\n", argv[1]);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	int size = ftell(file);
 	if (size == -1) {
 		fprintf(stderr, "(@): size error (%s)\n", argv[1]);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	if (fseek(file, 0, SEEK_SET) != 0) {
 		fprintf(stderr, "(@): seek error (%s)\n", argv[1]);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	char *prog = malloc(size);
 	if (prog == NULL) {
 		fprintf(stderr, "(@): alloc error (%dB)\n", size);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	if (fread(prog, 1, size, file) < size) {
 		fprintf(stderr, "(@): read error (%s)\n", argv[1]);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	if (fclose(file) != 0) {
 		fprintf(stderr, "(@): close error (%s)\n", argv[1]);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	int bulk = 1;
 	char *tape = malloc(bulk);
 	if (tape == NULL) {
 		fprintf(stderr, "(@): alloc error (%dB)\n", bulk);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	memset(tape, 0, bulk);
 	char *cell = tape;
-	int i = 0;
 	for (char *inst = prog; inst < prog + size; inst++) {
-		i++;
 		switch (*inst) {
 			case '>':
 				cell++;
@@ -56,8 +54,9 @@ int main(int argc, char **argv) {
 					char *swap = realloc(tape, bulk * 2);
 					if (swap == NULL) {
 						fprintf(stderr, "(@): realloc error (%dB)\n", bulk * 2);
-						return EXIT_FAILURE;
+						return 1;
 					}
+					memset(swap + bulk, 0x00, bulk);
 					cell += swap - tape;
 					tape = swap;
 					bulk *= 2;
@@ -69,10 +68,10 @@ int main(int argc, char **argv) {
 					char *swap = realloc(tape, bulk * 2);
 					if (swap == NULL) {
 						fprintf(stderr, "(@): realloc error (%dB)\n", bulk * 2);
-						return EXIT_FAILURE;
+						return 1;
 					}
 					memcpy(swap + bulk, swap, bulk);
-					memset(swap, 0, bulk);
+					memset(swap, 0x00, bulk);
 					cell += swap - tape + bulk;
 					tape = swap;
 					bulk *= 2;
@@ -80,15 +79,9 @@ int main(int argc, char **argv) {
 				break;
 			case '+':
 				(*cell)++;
-				if (*cell >= 0x100) {
-					*cell %= 0x100;
-				}
 				break;
 			case '-':
 				(*cell)--;
-				if (*cell < 0) {
-					*cell %= 0x100;
-				}
 				break;
 			case '[':
 				if (*cell == 0) {
@@ -124,18 +117,9 @@ int main(int argc, char **argv) {
 			case ',':
 				*cell = fgetc(stdin);
 				break;
-			case '#':
-				printf("position within code: %d\n", (int) (inst - prog));
-				printf("pointer: %d\n", (int) (cell - tape));
-				printf("value at pointer: %d\n", (int) *cell);
-				break;
-			default:
-				i--;
-				break;
 		}
 	}
-	printf("finished instructions: %d\n", i);
 	free(prog);
 	free(tape);
-	return EXIT_SUCCESS;
+	return 0;
 }
