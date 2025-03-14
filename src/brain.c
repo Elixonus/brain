d243475e1f98947c3a4f62840941726cb63f9941
Elias Dobrin <elias@dobrin.net>
2025-03-13 21:58:37 -0700

    changed code curly braces style

--- src/brain.c
+++ src/brain.c
@@ -2,57 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
-	if (argc != 2) {
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
 		fprintf(stderr, "(@): call error (%s)\n", argv[0]);
 		return 1;
 	}
 	FILE *file = fopen(argv[1], "r");
-	if (file == NULL) {
+	if(file == NULL)
+	{
 		fprintf(stderr, "(@): open error (%s)\n", argv[1]);
 		return 1;
 	}
-	if (fseek(file, 0, SEEK_END) != 0) {
+	if(fseek(file, 0, SEEK_END) != 0)
+	{
 		fprintf(stderr, "(@): seek error (%s)\n", argv[1]);
 		return 1;
 	}
 	int size = ftell(file);
-	if (size == -1) {
+	if(size == -1)
+	{
 		fprintf(stderr, "(@): size error (%s)\n", argv[1]);
 		return 1;
 	}
-	if (fseek(file, 0, SEEK_SET) != 0) {
+	if(fseek(file, 0, SEEK_SET) != 0)
+	{
 		fprintf(stderr, "(@): seek error (%s)\n", argv[1]);
 		return 1;
 	}
 	char *prog = malloc(size);
-	if (prog == NULL) {
+	if(prog == NULL)
+	{
 		fprintf(stderr, "(@): alloc error (%dB)\n", size);
 		return 1;
 	}
-	if (fread(prog, 1, size, file) < size) {
+	if(fread(prog, 1, size, file) < size)
+	{
 		fprintf(stderr, "(@): read error (%s)\n", argv[1]);
 		return 1;
 	}
-	if (fclose(file) != 0) {
+	if(fclose(file) != 0)
+	{
 		fprintf(stderr, "(@): close error (%s)\n", argv[1]);
 		return 1;
 	}
 	int bulk = 1;
 	char *tape = malloc(bulk);
-	if (tape == NULL) {
+	if(tape == NULL)
+	{
 		fprintf(stderr, "(@): alloc error (%dB)\n", bulk);
 		return 1;
 	}
 	memset(tape, 0, bulk);
 	char *cell = tape;
-	for (char *inst = prog; inst < prog + size; inst++) {
-		switch (*inst) {
+	for(char *inst = prog; inst < prog + size; inst++)
+	{
+		switch (*inst)
+		{
 			case '>':
 				cell++;
-				if (cell >= tape + bulk) {
+				if(cell >= tape + bulk)
+				{
 					char *swap = realloc(tape, bulk * 2);
-					if (swap == NULL) {
+					if(swap == NULL)
+					{
 						fprintf(stderr, "(@): realloc error (%dB)\n", bulk * 2);
 						return 1;
 					}
@@ -64,9 +78,11 @@ int main(int argc, char **argv) {
 				break;
 			case '<':
 				cell--;
-				if (cell < tape) {
+				if(cell < tape)
+				{
 					char *swap = realloc(tape, bulk * 2);
-					if (swap == NULL) {
+					if(swap == NULL)
+					{
 						fprintf(stderr, "(@): realloc error (%dB)\n", bulk * 2);
 						return 1;
 					}
@@ -84,29 +100,33 @@ int main(int argc, char **argv) {
 				(*cell)--;
 				break;
 			case '[':
-				if (*cell == 0) {
-					for (int loop = 0; inst < prog + size; inst++) {
-						if (*inst == '[') {
+				if(*cell == 0)
+				{
+					for(int loop = 0; inst < prog + size; inst++)
+					{
+						if(*inst == '[')
 							loop++;
-						} else if (*inst == ']') {
+						else if(*inst == ']')
+						{
 							loop--;
-							if (loop == 0) {
+							if(loop == 0)
 								break;
-							}
 						}
 					}
 				}
 				break;
 			case ']':
-				if (*cell != 0) {
-					for (int loop = 0; inst >= prog; inst--) {
-						if (*inst == ']') {
+				if(*cell != 0)
+				{
+					for(int loop = 0; inst >= prog; inst--)
+					{
+						if(*inst == ']')
 							loop++;
-						} else if (*inst == '[') {
+						else if(*inst == '[')
+						{
 							loop--;
-							if (loop == 0) {
+							if(loop == 0)
 								break;
-							}
 						}
 					}
 				}
